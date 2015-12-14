547ef3df89131919640d613baf11e71df07cc63c
JingWei <jw@zovei.cn>
2015-12-14 14:50:14 +0800

    无限极下拉菜单-2,基本完成了这个功能

--- jquery.Pocket.js
+++ jquery.Pocket.js
@@ -1,16 +1,50 @@
-﻿;(function($){
+﻿/*! pocket v0.0.1 网页特效合集 License LGPL  http://fmwei.com By FM7 */
+/*!
+ @Name：pocket v0.0.1 网页特效合集
+ @Author：FM7
+ @Site：http://fmwei.com
+ @License：LGPL
+ */
+;(function($){
     $.fn.nav = function(options) { //定义插件的名称，这里为userCp
         var defaults = {
             //以下为该插件的属性及其默认值
-            dom: '.son-nav', //子菜单
-            showtime: 300, //子菜单
-            hidetime: 200, //子菜单
+            dom: '.son-nav', //自定义子菜单
+            showtime: 300, //显示动画过渡速度
+            hidetime: 200, //隐藏动画过渡速度
+            type: 'down',
         };
         var ops = $.extend(defaults,options);
         $(this).find(ops.dom).parent().hover(function() {
-            $(this).children(ops.dom).slideDown(ops.showtime);
+            switch(ops.type){
+                case 'down':
+                    $(this).children(ops.dom).slideDown(ops.showtime);
+                break;
+                case 'left':
+                    $(this).children(ops.dom).show(ops.showtime);
+                break;
+                case 'fade':
+                    $(this).children(ops.dom).fadeIn(ops.showtime);
+                break;
+                default:
+                    $(this).children(ops.dom).slideDown(ops.showtime);
+                break;
+            }
         }, function() {
-            $(this).children(ops.dom).stop().slideUp(ops.hidetime);
+            switch(ops.type){
+                case 'down':
+                    $(this).children(ops.dom).stop().slideUp(ops.hidetime);
+                break;
+                case 'left':
+                    $(this).children(ops.dom).hide(ops.showtime);
+                break;
+                case 'fade':
+                    $(this).children(ops.dom).fadeOut(ops.showtime);
+                break;
+                default:
+                    $(this).children(ops.dom).stop().slideUp(ops.hidetime);
+                break;
+            }
         });
     };
 
