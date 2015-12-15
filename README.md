c5ee603c0807cb7af0d97206c4cb6181a8ab9ba9
JingWei <jw@zovei.cn>
2015-12-15 16:23:41 +0800

    完成了信息提示和文本滚动-1

--- README.md
+++ README.md
@@ -13,10 +13,41 @@
         type: 'down', //动画效果，默认为down,有down、left、fade 3种
     });
 ```
-------
+### 2、文本滚动
+```javascript
+    // 示例1
+    $('.p-news-ul').textSlider(700);
+```
+
+### 3、提示弹框
+```javascript
+    // 示例1
+    msg('我是提示');
+
+    // 示例2
+    msg('我是提示',{
+        background: '#000', //设置背景颜色
+        opacity: '0.6', //设置透明度
+        width: '120px', // 设置宽度
+        height: '50px', //设置高度
+        color: '#fff', //设置文字颜色
+        radius: '3px', //圆角
+        icon :1, //图标,1-11可选
+        time: 3000, //显示时长
+        top: '50%', //默认为居中，通过该属性可以调整弹窗位置
+    });
+
+    // 示例3
+    var config = {
+        icon:10,
+        width:'200px',
+        top:'10px',
+    };
+    msg('我是文字',config);
+```
+
 ### 4、返回顶部
 ```javascript
     // 示例1
      $('#p-top').top();
 ```
-------
--- jquery.pocket.js
+++ jquery.pocket.js
@@ -6,6 +6,7 @@
  */
 
 ;(function($){
+    // 1、无限极菜单
     $.fn.nav = function(options) {
         var defaults = {
             dom: '.son-nav',
@@ -45,6 +46,90 @@
                 break;
             }
         });
+    };
+
+// 文本滚动
+    $.fn.textSlider = function(options) {
+        var time = options ? time=options : time=2000;
+        var ul = $(this);
+        var scrollUp=function(){
+            ul.animate({
+                marginTop : -ul.find('li').height()
+            },300,function(){
+                ul.css({marginTop : "0px"}).find("li:first").appendTo(ul);
+            });
+        };
+        setInterval(scrollUp,time);
+    };
+
+
+// 提示弹框
+    msg = function(msg,options) {
+        var defaults = {
+            background: '#000',
+            opacity: '0.6',
+            width: '150px',
+            height: '50px',
+            color: '#fff',
+            radius: '3px',
+            icon :0,
+            time: 3000,
+            top: '50%',
+        };
+        var ops = $.extend(defaults,options);
+        var width = /^\d+/.exec(ops.width);
+        var heights = /^\d+/.exec(ops.height);
+        var height = ops.top=='50%' ? height=heights : height='0';
+
+        var iconn = 'inline-block';
+        switch(ops.icon){
+            case 1:
+                var icon = '../img/msg/1.png'
+            break;
+            case 2:
+                var icon = '../img/msg/2.png'
+            break;
+            case 3:
+                var icon = '../img/msg/3.png'
+            break;
+            case 4:
+                var icon = '../img/msg/4.png'
+            break;
+            case 5:
+                var icon = '../img/msg/5.png'
+            break;
+            case 6:
+                var icon = '../img/msg/6.png'
+            break;
+            case 7:
+                var icon = '../img/msg/7.png'
+            break;
+            case 8:
+                var icon = '../img/msg/l1.gif'
+            break;
+            case 9:
+                var icon = '../img/msg/l2.gif'
+            break;
+            case 10:
+                var icon = '../img/msg/l3.gif'
+            break;
+            case 11:
+                var icon = '../img/msg/l4.gif'
+            break;
+            default:
+                var iconn = 'none';
+            break;
+        }
+
+        if($('.p-msg').length<=0){
+            var data = '<div class="p-msg" style="background:'+ops.background+';opacity:'+ops.opacity+';width:'+ops.width+';height:'+ops.height+';line-height:'+ops.height+';color:'+ops.color+';border-radius:'+ops.radius+';top:'+ops.top+';margin-left:-'+width/2+'px;margin-top:-'+height/2+'px;"><i style="background-image:url('+icon+');display:'+iconn+'"></i>'+msg+'</div>';
+                    $('body').append(data);
+                    $('.p-msg').fadeIn();
+                    setTimeout("$('.p-msg').remove()", ops.time);
+        }else{
+            return;
+        }
+
     };
 
 //返回顶部
